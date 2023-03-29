15fde4738f3b573beaf56f28a7bee46c8c3bb706
Oneiric0102 <ndmjlove2@gmail.com>
2023-03-29 19:02:10 +0900

    signIn#2

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -15,64 +15,63 @@ function SignInPage() {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");//string
   const [password, setPassword] = useState("");//string
-  const [loggedIn, setLoggedIn] = useState(false);//bool
 
   const onPressLogin = () => {
-    setLoggedIn(!loggedIn);
-  }
-
-  const onPressLogout = () => {
-    setLoggedIn(!loggedIn);
-    setEmail("");
-    setPassword("");
+    navigation.navigate('ProgramListPage');
   }
 
   const onPressJoin = () => {
     navigation.navigate('SignUpPage');
   }
 
+  const onPressPwdSearch = () => {
+
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTxt}>로그인</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>이메일</Text>
+          <TextInput 
+            value={email} 
+            onChangeText={(email) => setEmail(email)} 
+            keyboardType="email-address" 
+            autoCapitalize="none" 
+            style={styles.input}
+            placeholder="Email address" 
+          />
       </View>
-      <View style={styles.main}>
-        {
-          loggedIn ? 
-          <View style={styles.headerTxt}>
-            <Text>로그인 되었습니다.</Text>
-            <Text>email: {email}</Text>
-            <Text>비밀번호: {password}</Text>
-            <TouchableOpacity onPress={onPressLogout}>
-              <Text style={styles.login}>로그아웃</Text>
-            </TouchableOpacity>
-          </View> :
-          <View>
-            <View style={styles.box}>
-              <Text>이메일</Text>
-              <TextInput value={email} onChangeText={(email) => setEmail(email)} keyboardType="email-address" autoCapitalize="none" style={styles.input}/>
-            </View>
-            <View style={styles.box}>
-              <Text>비밀번호</Text>
-              <TextInput value={password} onChangeText={(password) => setPassword(password)} secureTextEntry={true} autoCapitalize="none" style={styles.input}/>
-            </View>
-            <TouchableOpacity onPress={onPressLogin}>
-              <Text style={styles.login}>로그인</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={onPressJoin}>
-              <Text style={styles.join}>회원가입</Text>
-            </TouchableOpacity>
-            <View style={{flexDirection: "row", justifyContent: 'center'}}>
-              <TouchableOpacity style={styles.kakao}>
-                <Text>카카오 로그인</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.google}>
-                <Text>구글 로그인</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        }
-      </View>      
+      <View style={styles.box}>
+        <Text style={styles.text}>비밀번호</Text>
+        <TextInput 
+          value={password} 
+          onChangeText={(password) => setPassword(password)} 
+          secureTextEntry={true} 
+          autoCapitalize="none" 
+          style={styles.input}
+          placeholder="Password"  
+        />
+      </View>
+      <TouchableOpacity onPress={onPressLogin}>
+        <Text style={styles.login}>로그인</Text>
+      </TouchableOpacity>
+      <View style={{flexDirection: "row", justifyContent: 'center'}}>
+        <TouchableOpacity onPress={onPressJoin}>
+          <Text style={styles.txtButton}>회원가입</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onPressPwdSearch}>
+          <Text style={styles.txtButton}>비밀번호 찾기</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.VertialLine}></View>
+      <View>
+        <TouchableOpacity style={styles.kakao}>
+          <Text style={styles.kakaoTxt}>카카오 로그인</Text>
+        </TouchableOpacity>
+        <TouchableOpacity  style={styles.google}>
+          <Text style={styles.googleTxt}>Google 계정으로 로그인</Text>
+        </TouchableOpacity>
+      </View>     
     </View>
   );
 }
@@ -81,96 +80,113 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-  },
-
-  header: {
-    flex: 1,
-    borderBottomColor: "darkgray",
-    borderBottomWidth: 1,
     justifyContent: "center",
-    alignItems: "flex-start",
-    marginTop: 20,
-  },
-
-  headerTxt: {
-    fontSize: 26,
-    marginLeft: 20,
-  },
-
-  main: {
-    flex: 10,
-    marginTop: 100,
-  },
-
-  input: {
-    backgroundColor: "white",
-    borderColor: "darkgray",
-    borderWidth: 1,
-    fontSize:18,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
+    alignItems: "center",
   },
 
   box: {
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 20,
+    margin: 5,
+  },
+
+  text: {
+    fontSize: 18,
+    color: "#170F49",
+    paddingLeft: 18,
+  }, 
+
+  input: {
+    backgroundColor: '#fff',
+    width: 300,
+    height: 55,
+    borderWidth: 1,
+    borderColor: "#EFF0F6",
+    borderRadius: 46,
+    fontSize: 18,
+    color: "#6F6C90",
+    paddingLeft: 18,
+    margin: 10,
   },
 
   login: {
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 30,
-    marginBottom: 10,
-    paddingVertical: 20,
-    paddingHorizontal: 10,
-    borderWidth:1,
-    borderColor: "black",
-    borderRadius: 10,
-    textAlign: "center",
+    backgroundColor: '#4A3AFF',
+    width: 300,
+    height: 55,
+    borderRadius: 46,
     fontSize: 18,
+    color: "#fff",
+    margin: 10,
+    textAlign: "center",
+    textAlignVertical: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 
-  join: {
-    marginLeft: 30,
-    marginRight: 30,
-    marginBottom: 10,
-    paddingVertical: 20,
-    paddingHorizontal: 10,
-    borderWidth:1,
-    borderColor: "black",
-    borderRadius: 10,
-    textAlign: "center",
-    fontSize: 18,
+  txtButton: {
+    color: "#6F6C90",
+    fontSize: 16,
+    marginHorizontal: 25,
+    marginVertical: 5,
+  },
+
+  VertialLine: {
+    width: 300,
+    borderBottomColor: "#EFF0F6",
+    borderBottomWidth: 1,
+    marginVertical: 20,
   },
 
   kakao: {
-    marginVertical: 30,
-    marginLeft: 30,
-    marginRight:5,
-    fontSize: 18,
-    width:140,
-    height:70,
+    backgroundColor: "#FEE500",
+    width: 300,
+    height: 55,
+    borderRadius: 46,
+    margin: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+    elevation: 5,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "yellow",
-    borderRadius: 10,
+  },
+
+  kakaoTxt: {
+    fontSize: 18,
+    color: "#000000",
+    opacity: 0.85,
+    textAlign: "center",
   },
 
   google: {
-    marginVertical: 30,
-    marginLeft: 5,
-    marginRight:30,
-    width:140,
-    height:70,
+    backgroundColor: "#fff",
+    width: 300,
+    height: 55,
+    borderRadius: 46,
+    margin: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+    elevation: 5,
     justifyContent: "center",
-    alignItems: "center",
-    borderWidth:1,
-    borderColor: "lightgray",
-    borderRadius: 10,
   },
+
+  googleTxt: {
+    fontSize: 18,
+    color: "#000000",
+    textAlign: "center",
+  }
 });
 
 export default SignInPage;
